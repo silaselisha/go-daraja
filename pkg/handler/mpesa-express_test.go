3fe8c3be6dcd23644c7e172ada3d4a7a8c42851a
Elisha <elisilas87@outlook.com>
2024-03-16 17:21:15 +0300

    daraja auth to receive consumer key/secret as arguments

--- pkg/handler/mpesa-express_test.go
+++ pkg/handler/mpesa-express_test.go
@@ -4,23 +4,26 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/silaselisha/go-daraja/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMpesaExpress(t *testing.T) {
 	testCases := []struct {
-		name        string
-		phoneNumber string
-		description string
-		amount      float64
-		check       func(t *testing.T, data []byte, err error)
+		name           string
+		phoneNumber    string
+		description    string
+		amount         float64
+		consumerKey    string
+		consumerSecret string
+		check          func(t *testing.T, data []byte, err error)
 	}{
 		{
-			name:        "valid transaction",
-			phoneNumber: "0708374148",
-			description: "test payment",
-			amount:      1,
+			name:           "valid transaction",
+			phoneNumber:    "0708374148",
+			description:    "test payment",
+			amount:         1,
+			consumerKey:    "JDG40OnpvvRgXhgoPZ9GhGCTm1WZ42geJ66pH1tHIwwo4MrR",
+			consumerSecret: "yQcMx6pBUMVjZ90ILmA3QGJzf0m0l2gwhY45l9S3EzcLkH8xOPdqIaE7DQiX5xyO",
 			check: func(t *testing.T, data []byte, err error) {
 
 				var payload StkCallback
@@ -33,10 +36,12 @@ func TestMpesaExpress(t *testing.T) {
 			},
 		},
 		{
-			name:        "invalid mpesa [PHONE NUMBER] transaction",
-			phoneNumber: "070837414",
-			description: "test payment",
-			amount:      1,
+			name:           "invalid mpesa [PHONE NUMBER] transaction",
+			phoneNumber:    "070837414",
+			description:    "test payment",
+			amount:         1,
+			consumerKey:    "JDG40OnpvvRgXhgoPZ9GhGCTm1WZ42geJ66pH1tHIwwo4MrR",
+			consumerSecret: "yQcMx6pBUMVjZ90ILmA3QGJzf0m0l2gwhY45l9S3EzcLkH8xOPdqIaE7DQiX5xyO",
 			check: func(t *testing.T, data []byte, err error) {
 
 				require.Error(t, err)
@@ -46,8 +51,7 @@ func TestMpesaExpress(t *testing.T) {
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			authToken := util.GenAuthorizationToken(testEnvs.DarajaConsumerKey, testEnvs.DarajaConsumerSecret)
-			auth, err := NewDarajaAuth(authToken)
+			auth, err := NewDarajaAuth(test.consumerKey, test.consumerSecret)
 			require.NoError(t, err)
 
 			payload, err := auth.MpesaExpress(test.description, test.phoneNumber, test.amount)
